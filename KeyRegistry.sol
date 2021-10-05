581b2baa7cb41dbb98646bd3283b6061e7b93b44
Thomas BRILLARD <thomb45@gmail.com>
2021-10-05 13:43:43 +0200

    Update KeyRegistry.sol

--- KeyRegistry.sol
+++ KeyRegistry.sol
@@ -1,14 +1,37 @@
 pragma solidity >=0.8.0;
 
-contract RevocationList {
 
-    mapping(bytes32 => bool) revocationList;
+// Chambre des metiers ou
+contract KeyRegistry {
 
-    function revocateDocument(bytes32 didPreuve) public {
-        revocationList[keccak256(abi.encodePacked(msg.sender, didPreuve))] = true;
+    mapping(bytes32 => address) controllerKeys; // keys to manage authorisation on the registry
+    mapping(bytes32 => mapping(bytes32 => bool)) signaturesKey; //did:craftman => BLS12-381 public key => true
+    address owner;
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "only owner is allowed to perform action");
+        _;
     }
 
-    function isDocumentRevocated(bytes32 didPreuve, address controller) public view returns (bool){
-        return revocationList[keccak256(abi.encodePacked(controller, didPreuve))];
+    constructor(address owner_) {
+        owner = owner_;
+    }
+
+    // check if the key is registered onchain
+    function validateKey(bytes32 did, bytes32 key) public view returns (bool) {
+        return signaturesKey[did][key];
+    }
+
+    // add a public key to the registry
+    // must be allowed to interact with the did
+    function addKey(bytes32 did, bytes32 key) public {
+        require( controllerKeys[did] == msg.sender);
+        signaturesKey[did][key] = true;
+    }
+
+    // authorize an Ethereum key to add BLS key to the registry
+    function createId(bytes32 did, address addr) onlyOwner public {
+        controllerKeys[did] = addr;
     }
 }
